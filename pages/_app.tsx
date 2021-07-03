37bed1b2589df6e9efedc37be4d04a49657adab0
Richard Hofmänner <hello@terillos.dev>
2021-07-03 20:40:41 +0200

    Added Landingpage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Home | todoo.xyz</title>
+        <link rel="icon" href="/images/favicon.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 export default MyApp
